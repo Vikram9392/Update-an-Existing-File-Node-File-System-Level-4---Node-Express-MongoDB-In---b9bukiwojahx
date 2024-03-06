43d14fde346558210204d0a5313732c7b3d1b826
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 12:35:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
+const fileContent = ',An online learning platform';
 const updateFile = async (fileName, fileContent) => {
-  // write code here
-  // dont change function name
+  fs.writeFile(fileName,fileContent)
   
 };
+updateFile(fileName,fileContent);
 
 module.exports = updateFile;
